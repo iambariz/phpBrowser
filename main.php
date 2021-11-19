d8cc48fb1b41f5867f0e46cc03ab613b37177768
David Barkanyi <61651210+iambariz@users.noreply.github.com>
2021-11-19 12:16:34 +0000

    Styling

--- main.php
+++ main.php
@@ -3,10 +3,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
+
+    <style>
+        th:nth-of-type(2) {
+            border-width: 0px 1px 0px 1px;
+            border-color: #000;
+            border-style: solid;
+        }
+
+        th {
+            width: 10rem;
+        }
+    </style>
 </head>
 
 <body>
-    <p>Hello World</p>
+    <div class="main">
+        <table>
+            <tr>
+                <th>File Name</th>
+                <th>Date Modified</th>
+                <th>Type</th>
+            </tr>
+        </table>
+
+    </div>
 
 </body>
 
